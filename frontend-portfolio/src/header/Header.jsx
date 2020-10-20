d282a6c92ec2429bea52f78db9ab9e878d337485
matt_kuras <mattkuras@gmail.com>
2020-10-20 12:27:57 -0400

    create header with placeholder text

---
+++ frontend-portfolio/src/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+
+    return(
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Portfolio</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header

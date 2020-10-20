758b29935ced81aa232a479c9f6a1a040f005e7b
matt_kuras <mattkuras@gmail.com>
2020-10-20 12:16:49 -0400

    create-react-app and create home component

---
+++ frontend-portfolio/src/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+
+    return(
+        <div className='home-container'>
+            <h1>Matt Kuras</h1>
+            <ul>
+                <li>GitHub</li>
+                <li>LinkedIn</li>
+                <li>Resume</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home

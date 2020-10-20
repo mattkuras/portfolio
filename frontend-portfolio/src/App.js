a966b7e4d27ad9bae717ad8c412ce0d338bfc335
matt_kuras <mattkuras@gmail.com>
2020-10-20 12:29:04 -0400

    render header, home, and portfolio componentns

--- frontend-portfolio/src/App.js
+++ frontend-portfolio/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './home/Home'
+import Header from './header/Header'
+import Portfolio from './portfolio/Portfolio'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Home/>
+      <Portfolio/>
     </div>
   );
 }

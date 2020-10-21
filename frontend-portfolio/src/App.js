bde82077f28e8fca237122a61bd75dc6b7ab235a
matt_kuras <mattkuras@gmail.com>
2020-10-21 10:02:40 -0400

    create about component and add to App

--- frontend-portfolio/src/App.js
+++ frontend-portfolio/src/App.js
@@ -4,12 +4,13 @@ import Home from './components/home/Home'
 import Header from './components/header/Header'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
-
+import About from './components/about/About'
 function App() {
   return (
     <div className="App">
       <Header/>
       <Home/>
+      <About/>
       <Portfolio/>
       <Contact/>
     </div>
---
+++ frontend-portfolio/src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='about-container'>
+            <h2>About Me</h2>
+            <p>Hello, I'm Matt Kuras. I'm a full-stack web developer.
+            </p>
+        </div>
+    )
+}
+
+export default About

d98a9a78102fb472d116253dea2aa762f2731b49
matt_kuras <mattkuras@gmail.com>
2020-10-22 10:24:54 -0400

    style home and about

--- frontend-portfolio/src/components/about/About.jsx
+++ frontend-portfolio/src/components/about/About.jsx
@@ -1,24 +1,56 @@
 import React from 'react'
 import './About.css'
+import {DiHtml5, DiReact} from 'react-icons/di'
+import {DiRuby} from 'react-icons/di'
+import {SiRails} from 'react-icons/si'
+import {DiJavascript1} from 'react-icons/di'
+import {SiRedux} from 'react-icons/si'
+import {AiOutlineHtml5} from 'react-icons/ai'
+import {DiCss3Full} from 'react-icons/di'
+import {SiRedhat} from 'react-icons/si'
 
 const About = () => {
     return (
         <div className='about-container'>
-            <h2>About Me</h2>
-            <p>Hello, I'm Matt Kuras. I'm a full-stack web developer.</p>
-            <h2>Skills</h2>
-            <ul className='skills-list'>
-                <p><a href='https://reactjs.org/'>Ruby</a></p>
-                <p><a href='https://reactjs.org/'>Rails</a></p>
-                <p><a href='https://reactjs.org/'>Javascript</a></p>
-                <p><a href='https://reactjs.org/'>React</a></p>
-                <p><a href='https://reactjs.org/'>Redux</a></p>
-                <p><a href='https://reactjs.org/'>Sinatra</a></p>
-                <p><a href='https://reactjs.org/'>SQLite</a></p>
-                <p><a href='https://reactjs.org/'>HTML5</a></p>
-                <p><a href='https://reactjs.org/'>CSS3</a></p>
-            </ul>
-                
+            <div className='about-me'>
+                <h2>About Me</h2>
+                <p>Hello, I'm Matt Kuras. I'm a full-stack web developer.</p>
+            </div>
+            <h2 id='skills-header'>Skills</h2>
+            <div className='skills-container'>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><DiRuby/></a>
+                        <h2>Ruby</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><SiRails/></a>
+                        <h2>Rails</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><DiJavascript1/></a>
+                        <h2>Javascript</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><DiReact/></a>
+                        <h2>React</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><SiRedux/></a>
+                        <h2>Redux</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><SiRedhat/></a>
+                        <h2>Sinatra</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><AiOutlineHtml5/></a>
+                        <h2>HTML5</h2>
+                    </div>
+                    <div className='skill'>
+                        <a href='https://reactjs.org/'><DiCss3Full/></a>
+                        <h2>CSS3</h2>
+                    </div>
+            </div>
         </div>
     )
 }
--- frontend-portfolio/src/components/home/Home.jsx
+++ frontend-portfolio/src/components/home/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Home.css'
+import { FaGithub } from 'react-icons/fa'
+import {AiFillLinkedin} from 'react-icons/ai'
+import {CgFileDocument} from 'react-icons/cg'
 
 const Home = () => {
 
     return(
         <div className='home-container'>
-            <h1>Matt Kuras</h1>
-            <ul className='social-list'>
-                <li>GitHub</li>
-                <li>LinkedIn</li>
-                <li>Resume</li>
-            </ul>
+            <div className='home-content'>
+                <h1>Matt Kuras</h1>
+                <h3>Full Stack Web Developer</h3>
+                <ul className='social-list'>
+                    <li><a href='https://github.com/mattkuras' ><FaGithub/></a></li>
+                    <li><a href='https://www.linkedin.com/in/matthew-kuras-72a7711b5/'><AiFillLinkedin/></a></li>
+                    <li><a><CgFileDocument/></a></li>
+                </ul>
+            </div>
         </div>
     )
 }

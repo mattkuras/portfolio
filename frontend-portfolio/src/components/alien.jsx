9444ab438ca4eca732b8ce6ef6db0dcb116527ca
Matthew Kuras <mattkuras@gmail.com>
2021-11-14 11:24:45 -0600

    Create alien.jsx

---
+++ frontend-portfolio/src/components/alien.jsx
@@ -0,0 +1,39 @@
+import React, {useRef} from "react";
+import ReactPlayer from 'react-player'
+import { useState } from "react";
+
+
+function Alien() {
+
+ const [videoFilePath, setVideoFilePath] = useState("https://youtu.be/CewGoSigFe8");
+
+ const handleVideoUpload = (event) => {
+  setVideoFilePath(URL.createObjectURL(event.target.files[0]));
+  };
+
+
+  return (
+    
+    <div className="Alien" style={{height: "100%"}} >
+      
+      {/* <iframe
+        width="853"
+        height="480"
+        src={`../public/alien-dude.mp4`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      /> */}
+      <ReactPlayer playing={true}
+        url={videoFilePath}
+        width="100%"
+        height="100%"
+        controls={false}
+        loop={true}/>
+
+    </div>
+  );
+}
+
+export default Alien;
